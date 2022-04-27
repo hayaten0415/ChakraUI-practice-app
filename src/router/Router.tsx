8fa0ddc6a94f7ffff1b978b5437b10719831aac5
hayaten0415 <hayaten415@gmail.com>
2022-04-27 21:17:45 +0900

    create Header format

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import { Login } from "../components/pages/Login";
 import { NotFound } from "../components/pages/NotFound";
 import { HomeRoutes } from './HomeRoutes';
+import { HeaderLayout } from '../components/templates/HeaderLayout';
 
 export const Router: FC = memo(() => {
   return (
@@ -13,7 +14,7 @@ export const Router: FC = memo(() => {
         <Route
           key={route.path}
           path={"home" + route.path}
-          element={route.element}
+          element={<HeaderLayout>{route.element}</HeaderLayout>}
         />
       ))}
       <Route path="*" element={<NotFound />} />
