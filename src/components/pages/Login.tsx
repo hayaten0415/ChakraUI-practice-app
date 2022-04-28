32afc145aaee5ba1c7c23686cba99c4cac3ca22b
hayaten0415 <hayaten415@gmail.com>
2022-04-28 23:36:42 +0900

    create login view

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,20 @@
 import { memo, FC } from "react"
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react"
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: FC = memo(() => {
-  return <p>Login Page</p>
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          UserManagement App
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="user id" />
+          <PrimaryButton>Login</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 })
