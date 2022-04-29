26075432818cf58fe8c6e3ef2c0158e7ee85b723
hayaten0415 <hayaten415@gmail.com>
2022-04-29 22:12:16 +0900

    User List View

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,13 @@
 import { memo, FC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from '../organisms/user/UserCard';
 
 export const UserManagement: FC = memo(() => {
-  return <p>UserManagement Page</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard imageUrl="https://source.unsplash.com/random" userName="HOGE" fullName="HOGEHOGE" />
+      </WrapItem>
+    </Wrap>
+  );
 });
